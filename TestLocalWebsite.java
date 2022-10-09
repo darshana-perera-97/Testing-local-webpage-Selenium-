26835fe1c570d451e0e34e4c76dceaee6b848d68
Darshana Perera <darshana.saluka.10@gmail.com>
2022-10-09 12:25:33 +0530

    open the web site

--- TestLocalWebsite.java
+++ TestLocalWebsite.java
@@ -1,9 +1,14 @@
 package testLocalWebsite;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestLocalWebsite {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("file:///E:/Year 03 Semester 02/CS 3212/Activity 2022-09-07/index.html");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
 	}
 
